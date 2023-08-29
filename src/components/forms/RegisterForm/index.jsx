390e9d3e7de4081ba300a4ba12ab7af0ec4db2ea
Gustavo Lazarin <gustavolazarin03@gmail.com>
2023-08-29 12:04:54 -0300

    feat: add standard register form

--- src/components/forms/RegisterForm/index.jsx
+++ src/components/forms/RegisterForm/index.jsx
@@ -1,8 +1,19 @@
+import { Input } from "../inputs/Input";
+import { InputPassword } from "../inputs/InputPassword";
+
 export const RegisterForm = () => {
     return (
         <div>
-            <form action="">
-
+            <form>
+                <div>
+                    <Input type="text" placeholder="Nome"/>
+                    <Input type="email" placeholder="E-mail"/>
+                </div>
+                <div>
+                    <InputPassword placeholder="Senha"/>
+                    <InputPassword placeholder="Confirmar senha"/>
+                </div>
+                <button type="submit">Cadastrar-se</button>
             </form>
         </div>
     );
--- src/components/forms/inputs/InputPassword/index.jsx
+++ src/components/forms/inputs/InputPassword/index.jsx
@@ -2,7 +2,7 @@ export const InputPassword = ({label, id, ...rest}) => {
   return (
     <div>
       {label ? <label htmlFor={id}>{label}</label> : null}
-      <input ref={ref} type="password" id={id} {...rest}/>
+      <input type="password" id={id} {...rest}/>
     </div>
   );
 };
