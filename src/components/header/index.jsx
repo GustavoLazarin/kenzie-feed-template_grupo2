60e2fa42d387bb1a052b0e1d038e260dc5413999
Bruno Moleta <brunomoleta@pm.me>
2023-08-29 17:12:32 -0300

    feature/style

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -2,12 +2,12 @@ export const Header = () => {
   return (
     <header>
       
-      <div>
+      <div className="header">
         <img src="#" alt="KZ-logo" />
-        <div>
-          <button>Usuario</button>
-          <button>Acessar</button>
-          <button>Sair (icone)</button>
+        <div className="header__user">
+          <button className="btn__small btn__tertiary">Usuario</button>
+          <button className="btn__small btn__primary">Acessar</button>
+          <button className="btn__small btn__secondary">Sair (icone)</button>
         </div>
       </div>
     </header>
