ab67fc949bd23c965766df233e244d31f17de31a
João Pedro <jp871157@gmail.com>
2023-08-30 14:24:51 -0300

    feat: added new logic for rendering posts

--- src/pages/AllNewsPage/index.jsx
+++ src/pages/AllNewsPage/index.jsx
@@ -1,14 +1,17 @@
 import { Footer } from "../../components/Footer";
 import { NewsList } from "../../components/NewsList";
 import { Header } from "../../components/header";
+import { useNewsContext } from "../../providers/NewsContext";
 
 export const AllNewsPage = () => {
+	const { posts } = useNewsContext();
+
 	return (
 		<>
 			<Header />
 			<main>
 				<h1>Todas as notícias</h1>
-				<NewsList />
+				<NewsList newPosts={posts} />
 			</main>
 			<Footer />
 		</>
