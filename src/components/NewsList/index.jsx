140569bef5ac6e236ffab4219c20c5dd5094ceab
Bruno Moleta <brunomoleta@pm.me>
2023-08-30 17:47:01 -0300

    feat/style HomePage

--- src/components/NewsList/index.jsx
+++ src/components/NewsList/index.jsx
@@ -1,24 +1,24 @@
-import { NewsCard } from './NewsCard';
-import { useEffect, useState } from 'react';
-import { api } from '../../services/api';
+import { NewsCard } from "./NewsCard";
+import { useEffect, useState } from "react";
+import { api } from "../../services/api";
 
 export const NewsList = () => {
-	const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState([]);
 
-	useEffect(() => {
-		const getAllPosts = async () => {
-			const { data } = await api.get('posts?_embed=likes');
-			setPosts(data);
-		};
+  useEffect(() => {
+    const getAllPosts = async () => {
+      const { data } = await api.get("posts?_embed=likes");
+      setPosts(data);
+    };
 
-		getAllPosts();
-	}, []);
+    getAllPosts();
+  }, []);
 
-	return (
-		<ul>
-			{posts.map(post => (
-				<NewsCard key={post.id} post={post} />
-			))}
-		</ul>
-	);
+  return (
+    <ul className="grid news-gap">
+      {posts.map((post) => (
+        <NewsCard key={post.id} post={post} />
+      ))}
+    </ul>
+  );
 };
