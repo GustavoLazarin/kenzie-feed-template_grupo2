e6fd6265853f2a5c427bd36482f5f1ea76b130bc
Ramon Carvalho <destroit.com@outlook.com>
2023-08-30 15:15:40 -0300

    feat: implements useform and zod validation

--- src/components/forms/NewPostForm/index.jsx
+++ src/components/forms/NewPostForm/index.jsx
@@ -1,20 +1,41 @@
 import { useForm } from "react-hook-form"
 import { Input } from "../inputs/Input"
 import { TextArea } from "../inputs/TextArea"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { NewPostFormSchema } from "./NewPostFormSchema"
 
 export const NewPostForm = () => {
-    const { handleSubmit, register } = useForm({
+    const {
+        handleSubmit,
+        register,
+        formState: { errors },
+    } = useForm({
         mode: "onChange",
+        resolver: zodResolver(NewPostFormSchema),
     })
 
+    const submit = (formData) => {
+        console.log(formData)
+    }
+
     return (
-        <form>
-            <Input placeholder="Título" {...register("title")} />
-            <Input placeholder="Imagem em destaque" {...register("image")} />
+        <form onSubmit={handleSubmit(submit)}>
+            <Input
+                placeholder="Título"
+                {...register("title")}
+                error={errors.title}
+            />
+            <Input
+                placeholder="Imagem em destaque"
+                {...register("image")}
+                error={errors.image}
+            />
             <TextArea
                 placeholder="Conteúdo do post"
                 {...register("description")}
+                error={errors.description}
             />
+            <button type="submit">Criar post</button>
         </form>
     )
 }
