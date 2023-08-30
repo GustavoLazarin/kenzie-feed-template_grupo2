137017c1794cb96b4ab6eb35f0371915a75312d0
Bruno Moleta <brunomoleta@pm.me>
2023-08-30 18:41:10 -0300

    feat/style Sair Icon

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import { useUserContext } from "../../providers/UserContext";
-import Logo from '../../assets/KenzieFeed-Logo.svg';
-
+import Logo from "../../assets/KenzieFeed-Logo.svg";
+import { BiExit } from "react-icons/bi";
 
 export const Header = () => {
   const { logOut } = useUserContext();
@@ -13,14 +13,18 @@ export const Header = () => {
         <img src={Logo} alt="KZ-logo" />
         {token ? (
           <div className="header__user">
-            <button>Usuário</button>
-            <Link  to="/dashboard">
+            <button className="btn btn__primary btn__small">Usuário</button>
+            <Link className="btn btn__secondary btn__small" to="/dashboard">
               Dashboard
             </Link>
-            <button className="btn btn__small" onClick={logOut}>Sair (icone)</button>
+            <button className="btn btn__small btn__exit" onClick={logOut}> 
+              <BiExit size={25}/>
+            </button>
           </div>
         ) : (
-          <Link className="btn btn__small btn__primary w-auto" to="/login">Acessar</Link>
+          <Link className="btn btn__small btn__primary w-auto" to="/login">
+            Acessar
+          </Link>
         )}
       </div>
     </header>
