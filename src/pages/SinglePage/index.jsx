3144a8bd36922852044bf36b9c3d93bbe3ee313d
João Pedro <jp871157@gmail.com>
2023-08-30 11:37:23 -0300

    feat: added single news page basic structure

--- src/pages/SinglePage/index.jsx
+++ src/pages/SinglePage/index.jsx
@@ -1,14 +1,25 @@
 import { Footer } from "../../components/Footer";
+import { NewsList } from "../../components/NewsList";
 import { Header } from "../../components/header";
 
 export const SinglePage = () => {
-    return (
-        <div>
-            <Header/>
-            <section>
-
-            </section>
-            <Footer/>
-        </div>
-    )
+	return (
+		<>
+			<Header />
+			<main>
+				<article>
+					<span>Por: </span>
+					<h2></h2>
+					<img src="" alt="" />
+					<div>
+						<button></button>
+					</div>
+					<p></p>
+				</article>
+				<h1>Leia também</h1>
+				<NewsList />
+			</main>
+			<Footer />
+		</>
+	);
 };
