6f48a1d8e748b79e447d8c02e16781c87d96bb49
Bruno Moleta <brunomoleta@pm.me>
2023-08-30 12:22:12 -0300

    feat/style Header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -2,22 +2,24 @@ import { Link } from "react-router-dom";
 import { useUserContext } from "../../providers/UserContext";
 
 export const Header = () => {
-
   const { logOut } = useUserContext();
   const token = localStorage.getItem("@TOKEN");
 
   return (
     <header>
-      
       <div className="header">
         <img src="#" alt="KZ-logo" />
         {token ? (
-          <div>
-            <button>Usuario</button>
-            <Link to="/dashboard">Dashboard</Link>
-            <button onClick={logOut}>Sair (icone)</button>
-        </div>
-        ) : <Link to="/login">Acessar</Link>}
+          <div className="header__user">
+            <button>Usuário</button>
+            <Link  to="/dashboard">
+              Dashboard
+            </Link>
+            <button className="btn btn__small" onClick={logOut}>Sair (icone)</button>
+          </div>
+        ) : (
+          <Link className="btn btn__small btn__primary w-auto" to="/login">Acessar</Link>
+        )}
       </div>
     </header>
   );
