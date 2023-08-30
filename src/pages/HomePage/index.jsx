4516a6dbbfbacfba2a3edce83cdc6671de86f166
Bruno Moleta <brunomoleta@pm.me>
2023-08-30 17:48:26 -0300

    feat/style HomePage

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,29 +1,31 @@
-import { Footer } from '../../components/Footer';
-import { Header } from '../../components/header';
-import homeImg from '../../assets/homeImg.svg';
-import { Link } from 'react-router-dom';
-import { NewsList } from '../../components/NewsList';
+import { Footer } from "../../components/Footer";
+import { Header } from "../../components/header";
+import homeImg from "../../assets/homeImg.svg";
+import { Link } from "react-router-dom";
+import { NewsList } from "../../components/NewsList";
 
 export const HomePage = () => {
-	return (
-		<div>
-			<Header />
-			<main>
-				<section>
-					<h3>KENZIE FEED</h3>
-					<h1>Seja muito bem vindo ao KenzieFeed</h1>
-					<p>Fique por dentro das últimas notícias</p>
-					<img src={homeImg} alt="Kenzie Feed image" />
-				</section>
-				<section>
-					<div>
-						<h2>Últimas notícias</h2>
-						<Link to={'/news'}>Ver tudo</Link>
-					</div>
-					<NewsList />
-				</section>
-			</main>
-			<Footer />
-		</div>
-	);
+  return (
+    <div className="everything">
+      <Header />
+      <main className="content">
+        <section className="stack-large">
+          <p className="bold text-align-center">KENZIE FEED</p>
+          <h1 className="heading-2">Seja muito bem vindo ao KenzieFeed</h1>
+          <div className="stack-x-large">
+			<p className="text-align-center">Fique por dentro das últimas notícias</p>
+          <img src={homeImg} alt="Kenzie Feed image" />
+		  </div>
+        </section>
+        <section>
+          <div className="flex space-between">
+            <h2 className="heading-2">Últimas notícias</h2>
+            <Link className="btn__register btn btn__primary btn__small" to={"/news"}>Ver tudo</Link>
+          </div>
+          <NewsList />
+        </section>
+      </main>
+      <Footer />
+    </div>
+  );
 };
