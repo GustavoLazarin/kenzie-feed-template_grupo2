f8ac9bbdf9cc4ccbb14f23226a8f1a31f1c3cd29
jvpf <jvictorpf@outlook.com>
2023-08-30 16:18:37 -0300

    double image importation

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -5,7 +5,6 @@ import { Link } from "react-router-dom";
 import { NewsList } from "../../components/NewsList";
 import { useNewsContext } from "../../providers/NewsContext";
 import { Header } from '../../components/Header';
-import homeImg from '../../assets/homeImg.svg';
 
 export const HomePage = () => {
 	const { posts } = useNewsContext();
