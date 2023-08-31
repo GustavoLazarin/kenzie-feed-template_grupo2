5ecd27cf2c21ebb4b8b45b2ed946350e5a6e6faa
Gustavo Lazarin <gustavolazarin03@gmail.com>
2023-08-31 11:01:02 -0300

    feat: improved routes protection

--- src/Routes/RoutesMain.jsx
+++ src/Routes/RoutesMain.jsx
@@ -13,13 +13,13 @@ export const RoutesMain = () => {
 	return (
 		<Routes>
 			<Route element={<PublicRoutes />}>
-				<Route path="/news" element={<AllNewsPage />} />
-				<Route path="/news/:id" element={<SinglePage />} />
 				<Route path="/login" element={<LoginPage />} />
 				<Route path="/register" element={<RegisterPage />} />
 			</Route>
 
 			<Route path="/" element={<HomePage />} />
+			<Route path="/news" element={<AllNewsPage />} />
+			<Route path="/news/:id" element={<SinglePage />} />
 
 			<Route element={<PrivateRoutes />}>
 				<Route path="/dashboard" element={<DashboardPage />} />
