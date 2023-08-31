d42c7affe0d738a62979a56e207de388cbbe3257
João Pedro <jp871157@gmail.com>
2023-08-31 10:28:56 -0300

    feat: added scroll when click on read more

--- src/components/NewsList/NewsCard/index.jsx
+++ src/components/NewsList/NewsCard/index.jsx
@@ -4,21 +4,23 @@ import { useNewsContext } from "../../../providers/NewsContext";
 export const NewsCard = ({ post }) => {
 	const { setSinglePost } = useNewsContext();
 
-  return (
-    <li className="stack-large w-full">
-      <img className="w-full" src={post.image} alt={post.title} />
-      <div className="stack-medium">
-        <span className="author">Por: {post.owner}</span>
-        <h3 className="post-title">{post.title}</h3>
-		<div>
-
-        <Link className="btn__tertiary" to={`/news/${post.id}`} onClick={() => setSinglePost(post)}>
-				
-          Leia mais
-        
-			</Link>
-		</div>
-      </div>
-    </li>
-  );
+	return (
+		<li className="stack-large w-full">
+			<img className="w-full" src={post.image} alt={post.title} />
+			<div className="stack-medium">
+				<span className="author">Por: {post.owner}</span>
+				<h3 className="post-title">{post.title}</h3>
+				<div>
+					<Link
+						to={`/news/${post.id}`}
+						onClick={() => {
+							setSinglePost(post);
+							window.scrollTo(0, 0);
+						}}>
+						Leia mais
+					</Link>
+				</div>
+			</div>
+		</li>
+	);
 };
