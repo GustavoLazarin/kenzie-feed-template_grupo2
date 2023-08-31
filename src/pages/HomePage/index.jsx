3f9b8ac311dd5a5e92e84e127ccbfc5444453fef
Bruno Moleta <brunomoleta@pm.me>
2023-08-31 16:15:16 -0300

    feat/Style fix:Homepage

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -13,19 +13,19 @@ export const HomePage = () => {
                 <h3 className="min-w-full bold text-align-center">
                     KENZIE FEED
                 </h3>
-                <h1 className="heading-2">
+                <h1 className="heading-2 text-align-center min-w-full">
                     Seja muito bem vindo ao KenzieFeed
                 </h1>
-                <div className="stack-x-large">
+                <div className="stack-x-large ">
                     <p className="min-w-full text-align-center">
                         Fique por dentro das últimas notícias
                     </p>
                 </div>
             </section>
             <section className="min-w-full stack-x-large">
-                <div className="stack-2x-large grid justify-items-center">
+                <div className=" stack-2x-large grid justify-items-center">
                     <img src={homeImg} alt="Kenzie Feed image" />
-                    <div className="dashboard__heading">
+                    <div className="min-w-full overall-max-width align-items-center grid grid-heading">
                         <h2 className="heading-2 justify-self-start">
                             Últimas notícias
                         </h2>
