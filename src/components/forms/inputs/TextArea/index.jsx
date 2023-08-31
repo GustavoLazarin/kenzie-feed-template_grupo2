adcaa081b65b28aecda3cb0a76a5d1c40e1418f5
Bruno Moleta <brunomoleta@pm.me>
2023-08-31 18:05:43 -0300

    feat/style textarea in Modal

--- src/components/forms/inputs/TextArea/index.jsx
+++ src/components/forms/inputs/TextArea/index.jsx
@@ -2,9 +2,9 @@ import { forwardRef } from "react";
 
 export const TextArea = forwardRef(({label, id, error, ...rest}, ref) => {
   return (
-    <div>
+    <div >
       {label ? <label htmlFor={id}>{label}</label> : null}
-      <textarea ref={ref} id={id} {...rest}/>
+      <textarea rows="10" className="w-full" ref={ref} id={id} {...rest}/>
       {error ? <span>{error.message}</span> : null}
     </div>
   );
