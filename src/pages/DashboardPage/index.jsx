11f3e01ccb62484d266559b6674b544ba84c58e0
Bruno Moleta <brunomoleta@pm.me>
2023-08-31 11:34:45 -0300

    feat/style Dashboard

--- src/pages/DashboardPage/index.jsx
+++ src/pages/DashboardPage/index.jsx
@@ -1,34 +1,39 @@
-import { useEffect } from "react"
-import { Header } from "../../components/Header"
-import { Footer } from "../../components/Footer"
-import { UserNewsList } from "../../components/UserNewsList"
-import { MdAddCircleOutline } from "react-icons/md"
-import { useNewsContext } from "../../providers/NewsContext"
-import { NewPostModal } from "../../components/modals/NewPostModal"
+import { useEffect } from "react";
+import { Header } from "../../components/Header";
+import { Footer } from "../../components/Footer";
+import { UserNewsList } from "../../components/UserNewsList";
+import { MdAddCircleOutline } from "react-icons/md";
+import { useNewsContext } from "../../providers/NewsContext";
+import { NewPostModal } from "../../components/modals/NewPostModal";
 
 export const DashboardPage = () => {
-    const { getOwnPosts, isCreating, setIsCreating } = useNewsContext()
+  const { getOwnPosts, isCreating, setIsCreating } = useNewsContext();
 
-    // Efeito de montagem, executa 1x na montagem do componente
-    useEffect(() => {
-        getOwnPosts() //Faz uma requisição GET nos posts do usuário
-    }, [])
+  // Efeito de montagem, executa 1x na montagem do componente
+  useEffect(() => {
+    getOwnPosts(); //Faz uma requisição GET nos posts do usuário
+  }, []);
 
-    return (
-        <div>
-            <Header />
-            <section>
-                <div>
-                    <h2>Suas Publicações</h2>
-                    <button onClick={() => setIsCreating(true)}>
-                        <MdAddCircleOutline />
-                        Novo Post
-                    </button>
-                </div>
-                <UserNewsList />
-            </section>
-            <Footer />
-            {isCreating ? <NewPostModal /> : null}
+  return (
+    <div className="everything">
+      <Header />
+      <main className="w-full bg-grey grid justify-items-center">
+        <div className="padding-b-24 min-w-full overall-max-width grid justify-items-center stack-large">
+          <div className="dashboard__heading">
+            <h1 className="heading-3 justify-self-start">Suas Publicações</h1>
+            <button
+              className="btn justify-content-center flex align-items-center btn__primary  btn__max-width btn__newPost-gap "
+              onClick={() => setIsCreating(true)}
+            >
+              <MdAddCircleOutline />
+              Novo Post
+            </button>
+          </div>
+          <UserNewsList />
         </div>
-    )
-}
+      </main>
+      <Footer />
+      {isCreating ? <NewPostModal /> : null}
+    </div>
+  );
+};
