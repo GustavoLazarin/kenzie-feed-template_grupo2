e696988d2caae49e67b7fd6a3cd131ddb0d15c1f
Bruno Moleta <brunomoleta@pm.me>
2023-09-01 09:57:21 -0300

    feat/style fix:Dashboard List Position

--- src/pages/DashboardPage/index.jsx
+++ src/pages/DashboardPage/index.jsx
@@ -14,8 +14,8 @@ export const DashboardPage = () => {
 
   return (
     <div className="min-w-full content bg-grey">
-      <main className="w-full padding-b-24 bg-grey grid justify-items-center ">
-        <section className=" min-w-full overall-max-width grid justify-items-center stack-large">
+      <main className="h-full w-full padding-b-24 bg-grey grid justify-items-center ">
+        <section className="grid-template-row-auto-1 h-full min-w-full overall-max-width grid justify-items-center stack-large">
           <div className="w-full overall-max-width align-items-center grid grid-heading">
             <h1 className="heading-3 justify-self-start">Suas Publicações</h1>
             <button
@@ -27,6 +27,7 @@ export const DashboardPage = () => {
               Novo Post
             </button>
           </div>
+          
           <UserNewsList />
         </section>
       </main>
