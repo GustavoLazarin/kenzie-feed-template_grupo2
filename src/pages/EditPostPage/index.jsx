7d2a35a1145b653635532b47e1d76fdf3c69e372
Bruno Moleta <brunomoleta@pm.me>
2023-09-01 12:18:04 -0300

    fix/html fix:semantics

--- src/pages/EditPostPage/index.jsx
+++ src/pages/EditPostPage/index.jsx
@@ -1,11 +1,11 @@
-import { Navigate, useNavigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import { EditForm } from "../../components/forms/EditForm";
 import { useNewsContext } from "../../providers/NewsContext";
 import { AiOutlineArrowLeft } from "react-icons/ai";
 import { useDocTitle } from "../../hooks/useDocTitle";
+import { Link } from "react-router-dom";
 
 export const EditPostPage = () => {
-  const navigate = useNavigate();
   const { editingPost } = useNewsContext();
 
   useDocTitle("editar post")
@@ -16,13 +16,16 @@ export const EditPostPage = () => {
         <div className="w-full stack-large container__edit padding-i-24 padding-b-24 ">
           <div className="flex space-between ">
             <h1 className="bold post-title">Editando:</h1>
-            <button
-              className="btn__secondary btn bg-grey flex align-items-center btn__small btn__gap"
-              onClick={() => navigate("/dashboard")}
-            >
-              <AiOutlineArrowLeft size={22} />
-              Voltar
-            </button>
+            
+            <Link
+          className="btn__secondary btn bg-grey flex align-items-center btn__small btn__gap"
+          to="/dashboard"
+          aria-label="back"
+          title="voltar"
+        >
+          <AiOutlineArrowLeft size={21} />
+          Voltar
+        </Link>
           </div>
           <div>{editingPost ? <EditForm /> : <Navigate to="/dashboard" />}</div>
         </div>
