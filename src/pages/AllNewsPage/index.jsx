f65e867e0d9013f3dbd1e0c9c952135fba808b95
Ramon Carvalho <destroit.com@outlook.com>
2023-09-01 12:38:47 -0300

    fix: stop rendering dashboard link in dashboard page

--- src/pages/AllNewsPage/index.jsx
+++ src/pages/AllNewsPage/index.jsx
@@ -1,20 +1,19 @@
-import { NewsList } from "../../components/NewsList";
-import { useDocTitle } from "../../hooks/useDocTitle";
-import { useNewsContext } from "../../providers/NewsContext";
+import { NewsList } from "../../components/NewsList"
+import { useDocTitle } from "../../hooks/useDocTitle"
+import { useNewsContext } from "../../providers/NewsContext"
 
 export const AllNewsPage = () => {
-  const { posts } = useNewsContext();
-  useDocTitle("todas as notícias");
+    const { posts } = useNewsContext()
+    useDocTitle("todas as notícias")
 
-
-  return (
-    <>
-      <main>
-        <div className="stack-large overall-max-width">
-          <h1 className="heading-2">Todas as notícias</h1>
-          <NewsList newPosts={posts} />
-        </div>
-      </main>
-    </>
-  );
-};
+    return (
+        <>
+            <main>
+                <div className="stack-large overall-max-width">
+                    <h1 className="heading-2">Todas as notícias</h1>
+                    <NewsList newPosts={posts} />
+                </div>
+            </main>
+        </>
+    )
+}
