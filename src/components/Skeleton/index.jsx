550bd1907a6c2a04dcda41c3c185dc7285f27a48
Gustavo Lazarin <gustavolazarin03@gmail.com>
2023-09-01 12:00:18 -0300

    feat: add skeleton

---
+++ src/components/Skeleton/index.jsx
@@ -0,0 +1,12 @@
+export const Skeleton = () => {
+    return (
+    <div className="content overall-max-width form-gap">
+        <div className="skeleton line"></div>
+        <div className="skeleton lg"></div>
+        <div className="flex w-full form-gap">
+            <div className="skeleton md"></div>
+            <div className="skeleton md"></div>
+        </div>
+    </div>
+    )
+}
